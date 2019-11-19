677bb3c63b12dd9fabea6522bd3eff4a0c8aaca2
Laura Holstege <laholstege@ucdavis.edu>
2019-11-19 14:04:45 -0800

    setup knex and osrm

--- index.ts
+++ index.ts
@@ -1,5 +1,29 @@
+import dotenv from 'dotenv';
+import knex from 'knex';
+import OSRM from 'osrm';
+
 const main = async () => {
-  console.log('Hello World!');
+  dotenv.config();
+
+  console.log('connecting to db', process.env.DB_HOST);
+  // https://knexjs.org/
+  const pg = knex({
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'plumas-kmeans'
+    }
+  });
+
+  const osrm = new OSRM('./data/california-latest.osrm');
+  const options: OSRM.NearestOptions = {
+    coordinates: [[-121.716557, 38.592383]]
+  };
+  osrm.nearest(options, (err, response) => {
+    console.log(response.waypoints);
+  });
   return;
 };
 
