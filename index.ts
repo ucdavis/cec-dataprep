42a9ff077f1e2ce684c610bfbc9798461926bdc5
Laura Holstege <laholstege@ucdavis.edu>
2019-12-03 11:00:18 -0800

    add equations for frcs

--- index.ts
+++ index.ts
@@ -117,3 +117,78 @@ const sumBiomass = (pixel: Pixel) => {
     pixel.total3run_tpa_40_nomgt_2016_v20190630
   );
 };
+
+// these equations come from this sheet:
+// https://ucdavis.app.box.com/file/566320916282
+const calcRemovalsCT = (pixel: Pixel) => {
+  return (
+    pixel.total3run_tpa_0_nomgt_2016_v20190630 +
+    pixel.total3run_tpa_2_nomgt_2016_v20190630 +
+    pixel.total3run_tpa_7_nomgt_2016_v20190630 +
+    pixel.total3run_tpa_15_nomgt_2016_v20190630 +
+    pixel.total3run_sng_0_nomgt_2016_v20190630 +
+    pixel.total3run_sng_2_nomgt_2016_v20190630 +
+    pixel.total3run_sng_7_nomgt_2016_v20190630 +
+    pixel.total3run_sng_15_nomgt_2016_v20190630
+  );
+};
+
+const calcRemovalsSLT = (pixel: Pixel) => {
+  return (
+    pixel.total3run_tpa_15_nomgt_2016_v20190630 +
+    pixel.total3run_sng_15_nomgt_2016_v20190630
+  );
+};
+
+const calcRemovalsLLT = (pixel: Pixel) => {
+  return (
+    pixel.total3run_tpa_25_nomgt_2016_v20190630 +
+    pixel.total3run_tpa_35_nomgt_2016_v20190630 +
+    pixel.total3run_tpa_40_nomgt_2016_v20190630 +
+    pixel.total3run_sng_25_nomgt_2016_v20190630 +
+    pixel.total3run_sng_35_nomgt_2016_v20190630 +
+    pixel.total3run_sng_40_nomgt_2016_v20190630
+  );
+};
+
+const calcTreeVolCT = (pixel: Pixel) => {
+  return (
+    pixel.total3run_bmfol_0_nomgt_2016_v20190630 +
+    pixel.total3run_bmfol_2_nomgt_2016_v20190630 +
+    pixel.total3run_bmfol_7_nomgt_2016_v20190630 +
+    pixel.total3run_bmfol_15_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_0_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_2_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_7_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_15_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_0_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_2_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_7_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_15_nomgt_2016_v20190630
+    // TODO: add DBMCN and DBMSM
+  );
+};
+
+const calcTreeVolSLT = (pixel: Pixel) => {
+  return (
+    pixel.total3run_bmfol_15_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_15_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_15_nomgt_2016_v20190630
+    // TODO: add DBMCN 15 and DBMSM 15
+  );
+};
+
+const calcTreeVolLT = (pixel: Pixel) => {
+  return (
+    pixel.total3run_bmfol_25_nomgt_2016_v20190630 +
+    pixel.total3run_bmfol_35_nomgt_2016_v20190630 +
+    pixel.total3run_bmfol_40_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_25_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_35_nomgt_2016_v20190630 +
+    pixel.total3run_bmcwn_40_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_25_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_35_nomgt_2016_v20190630 +
+    pixel.total3run_bmstm_40_nomgt_2016_v20190630
+    // TODO: add DBMCN and DBMSM
+  );
+};
