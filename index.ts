33689457f652754f31f62d532075446768bda88d
Laura Holstege <laholstege@ucdavis.edu>
2019-12-12 16:29:02 -0800

    pretty

--- index.ts
+++ index.ts
@@ -22,9 +22,7 @@ const main = async () => {
   });
   const osrm = new OSRM('./data/california-latest.osrm');
 
-  const pixelsInCluster: Pixel[] = await pg
-    .table('plumas136')
-    .where({ cluster_no: 44127 });
+  const pixelsInCluster: Pixel[] = await pg.table('plumas136').where({ cluster_no: 44127 });
   const outputs = await processCluster(pixelsInCluster, osrm);
 
   console.log('destroying pg...');
@@ -35,114 +33,105 @@ main();
 
 const processCluster = async (pixels: Pixel[], osrm: OSRM) => {
   return new Promise(async (resolve, reject) => {
-  const centerOfBiomassSum = {
-    lat: 0,
-    lng: 0,
-    biomassSum: 0
-  };
-  pixels.forEach(pixel => {
-    const biomassInPixel = sumBiomass(pixel);
-    centerOfBiomassSum.lat += pixel.y * biomassInPixel;
-    centerOfBiomassSum.lng += pixel.x * biomassInPixel;
-    centerOfBiomassSum.biomassSum += biomassInPixel;
-  });
-  console.log('biomassSum: ' + centerOfBiomassSum.biomassSum);
-  const centerOfBiomassLat =
-    centerOfBiomassSum.lat / centerOfBiomassSum.biomassSum;
-  const centerOfBiomassLng =
-    centerOfBiomassSum.lng / centerOfBiomassSum.biomassSum;
-
-  console.log(
-    'center of biomass: [' +
-      centerOfBiomassLat +
-      ', ' +
-      centerOfBiomassLng +
-      ']'
-  );
-
-  const options: OSRM.NearestOptions = {
-    coordinates: [[centerOfBiomassLng, centerOfBiomassLat]]
-  };
-
-  console.log('finding nearest road to center of biomass:');
-  await osrm.nearest(options, async (err, response) => {
-    console.log('nearest road:');
-    console.log(response.waypoints);
-    const landing = {
-      latitude: response.waypoints[0].location[1],
-      longitude: response.waypoints[0].location[0]
+    const centerOfBiomassSum = {
+      lat: 0,
+      lng: 0,
+      biomassSum: 0
     };
-    console.log('landingElevation');
-    // TODO: pull this from db
-    const landingElevation = pixels.filter(
-      p =>
-        p.x > landing.longitude - 0.01 &&
-        p.x < landing.longitude + 0.01 &&
-        p.y > landing.latitude - 0.01 &&
-        p.y < landing.latitude + 0.01
-    )[0].elevation;
-    console.log('landingElevetion: ' + landingElevation);
-    console.log('pixel length: ' + pixels.length);
-    const area = pixels.length * 30 * 30 * 0.00024711; // pixels are 30m^2, area needs to be in acres
-    console.log('area is: ' + area + ' acres^2');
-    let totalFrcsOutputs: OutputVarMod = {
-      TotalPerAcre: 0,
-      TotalPerBoleCCF: 0,
-      TotalPerGT: 0
-    };
-    pixels.forEach(p => {
-      let distance = getPreciseDistance(landing, {
-        latitude: p.y,
-        longitude: p.x
-      });
-      distance = distance / 0.3048; // put in feet
-      const slope = Math.abs(((landingElevation - p.elevation) / distance)) * 100;
-      console.log('slope: ' + slope);
-
-      const frcsInput = {
-        System: 'Ground-Based Mech WT',
-        PartialCut: true,
-        DeliverDist: distance,
-        Slope: slope,
-        Elevation: p.elevation,
-        CalcLoad: true,
-        CalcMoveIn: true,
-        Area: area,
-        MoveInDist: 2,
-        CalcResidues: true,
-        UserSpecWDCT: 60,
-        UserSpecWDSLT: 58.6235,
-        UserSpecWDLLT: 62.1225,
-        UserSpecRFCT: 0,
-        UserSpecRFSLT: 0.25,
-        UserSpecRFLLT: 0.38,
-        UserSpecHFCT: 0.2,
-        UserSpecHFSLT: 0,
-        UserSpecHFLLT: 0,
-        RemovalsCT: calcRemovalsCT(p),
-        TreeVolCT: calcTreeVolCT(p),
-        RemovalsSLT: calcRemovalsSLT(p),
-        TreeVolSLT: calcTreeVolSLT(p),
-        RemovalsLLT: calcRemovalsLLT(p),
-        TreeVolLLT: calcTreeVolLLT(p)
-      };
-      console.log('FRCS INPUT: -------');
-      console.log(frcsInput);
-      const frcsOutput = runFrcs(frcsInput);
-      console.log('frcs output: ');
-      console.log(frcsOutput);
-      totalFrcsOutputs = {
-        TotalPerAcre: totalFrcsOutputs.TotalPerAcre + frcsOutput.TotalPerAcre,
-        TotalPerBoleCCF:
-          totalFrcsOutputs.TotalPerBoleCCF + frcsOutput.TotalPerBoleCCF,
-        TotalPerGT: totalFrcsOutputs.TotalPerGT + frcsOutput.TotalPerGT
-      };
+    pixels.forEach(pixel => {
+      const biomassInPixel = sumBiomass(pixel);
+      centerOfBiomassSum.lat += pixel.y * biomassInPixel;
+      centerOfBiomassSum.lng += pixel.x * biomassInPixel;
+      centerOfBiomassSum.biomassSum += biomassInPixel;
     });
-    console.log('total sum per acre * area: ');
-    console.log(totalFrcsOutputs.TotalPerAcre * area);
-    resolve(totalFrcsOutputs);
+    console.log('biomassSum: ' + centerOfBiomassSum.biomassSum);
+    const centerOfBiomassLat = centerOfBiomassSum.lat / centerOfBiomassSum.biomassSum;
+    const centerOfBiomassLng = centerOfBiomassSum.lng / centerOfBiomassSum.biomassSum;
+
+    console.log('center of biomass: [' + centerOfBiomassLat + ', ' + centerOfBiomassLng + ']');
+
+    const options: OSRM.NearestOptions = {
+      coordinates: [[centerOfBiomassLng, centerOfBiomassLat]]
+    };
+
+    console.log('finding nearest road to center of biomass:');
+    await osrm.nearest(options, async (err, response) => {
+      console.log('nearest road:');
+      console.log(response.waypoints);
+      const landing = {
+        latitude: response.waypoints[0].location[1],
+        longitude: response.waypoints[0].location[0]
+      };
+      console.log('landingElevation');
+      // TODO: pull this from db
+      const landingElevation = pixels.filter(
+        p =>
+          p.x > landing.longitude - 0.01 &&
+          p.x < landing.longitude + 0.01 &&
+          p.y > landing.latitude - 0.01 &&
+          p.y < landing.latitude + 0.01
+      )[0].elevation;
+      console.log('landingElevetion: ' + landingElevation);
+      console.log('pixel length: ' + pixels.length);
+      const area = pixels.length * 30 * 30 * 0.00024711; // pixels are 30m^2, area needs to be in acres
+      console.log('area is: ' + area + ' acres^2');
+      let totalFrcsOutputs: OutputVarMod = {
+        TotalPerAcre: 0,
+        TotalPerBoleCCF: 0,
+        TotalPerGT: 0
+      };
+      pixels.forEach(p => {
+        let distance = getPreciseDistance(landing, {
+          latitude: p.y,
+          longitude: p.x
+        });
+        distance = distance / 0.3048; // put in feet
+        const slope = Math.abs((landingElevation - p.elevation) / distance) * 100;
+        console.log('slope: ' + slope);
+
+        const frcsInput = {
+          System: 'Ground-Based Mech WT',
+          PartialCut: true,
+          DeliverDist: distance,
+          Slope: slope,
+          Elevation: p.elevation,
+          CalcLoad: true,
+          CalcMoveIn: true,
+          Area: area,
+          MoveInDist: 2,
+          CalcResidues: true,
+          UserSpecWDCT: 60,
+          UserSpecWDSLT: 58.6235,
+          UserSpecWDLLT: 62.1225,
+          UserSpecRFCT: 0,
+          UserSpecRFSLT: 0.25,
+          UserSpecRFLLT: 0.38,
+          UserSpecHFCT: 0.2,
+          UserSpecHFSLT: 0,
+          UserSpecHFLLT: 0,
+          RemovalsCT: calcRemovalsCT(p),
+          TreeVolCT: calcTreeVolCT(p),
+          RemovalsSLT: calcRemovalsSLT(p),
+          TreeVolSLT: calcTreeVolSLT(p),
+          RemovalsLLT: calcRemovalsLLT(p),
+          TreeVolLLT: calcTreeVolLLT(p)
+        };
+        console.log('FRCS INPUT: -------');
+        console.log(frcsInput);
+        const frcsOutput = runFrcs(frcsInput);
+        console.log('frcs output: ');
+        console.log(frcsOutput);
+        totalFrcsOutputs = {
+          TotalPerAcre: totalFrcsOutputs.TotalPerAcre + frcsOutput.TotalPerAcre,
+          TotalPerBoleCCF: totalFrcsOutputs.TotalPerBoleCCF + frcsOutput.TotalPerBoleCCF,
+          TotalPerGT: totalFrcsOutputs.TotalPerGT + frcsOutput.TotalPerGT
+        };
+      });
+      console.log('total sum per acre * area: ');
+      console.log(totalFrcsOutputs.TotalPerAcre * area);
+      resolve(totalFrcsOutputs);
+    });
   });
-});
 };
 
 const sumBiomass = (pixel: Pixel) => {
@@ -180,10 +169,7 @@ const calcRemovalsCT = (pixel: Pixel) => {
 };
 
 const calcRemovalsSLT = (pixel: Pixel) => {
-  return (
-    pixel.total3run_tpa_15_nomgt_2016_v20190630 +
-    pixel.total3run_sng_15_nomgt_2016_v20190630
-  );
+  return pixel.total3run_tpa_15_nomgt_2016_v20190630 + pixel.total3run_sng_15_nomgt_2016_v20190630;
 };
 
 const calcRemovalsLLT = (pixel: Pixel) => {
