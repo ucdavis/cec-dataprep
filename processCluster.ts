90e94778aea7d466f9bf576cecd109a3045acf31
Laura Holstege <laholstege@ucdavis.edu>
2019-12-20 10:56:31 -0800

    models + separate files

--- processCluster.ts
+++ processCluster.ts
@@ -1,43 +1,11 @@
 import { runFrcs } from '@ucdavis/frcs';
 import { OutputVarMod } from '@ucdavis/frcs/out/systems/frcs.model';
-import dotenv from 'dotenv';
 import { getPreciseDistance } from 'geolib';
 import knex from 'knex';
-import { Cluster } from 'models/cluster';
-import { HarvestCost } from 'models/harvestCost';
 import { Pixel } from 'models/pixel';
-import OSRM from 'osrm';
+import OSRM = require('osrm');
 
-const main = async () => {
-  dotenv.config();
-
-  console.log('connecting to db', process.env.DB_HOST);
-  // https://knexjs.org/
-  const pg = knex({
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: 'plumas-kmeans'
-    }
-  });
-  const osrm = new OSRM('./data/california-latest.osrm');
-  // const clusters: Cluster[] = await pg
-  //   .table('clusters')
-  //   .orderBy('id', 'random')
-  //   .limit(1);
-  // const cluster = clusters[0];
-  const pixelsInCluster: Pixel[] = await pg.table('pixels').where({ cluster_no: '44127' });
-  const outputs = await processCluster(pixelsInCluster, osrm, pg);
-
-  console.log('destroying pg...');
-  pg.destroy();
-};
-
-main();
-
-const processCluster = async (pixels: Pixel[], osrm: OSRM, pg: knex) => {
+export const processCluster = async (pixels: Pixel[], osrm: OSRM, pg: knex) => {
   return new Promise(async (resolve, reject) => {
     const centerOfBiomassSum = {
       lat: 0,
