bcb544ece913800ed2a618f79d51fd50b4d36235
Scott Kirkland <srkirkland@gmail.com>
2020-06-30 15:05:29 -0700

    update to use txn everywhere
    also add in some overrides for dbname and osrm file location

--- index.ts
+++ index.ts
@@ -23,57 +23,61 @@ const processAllTreatments = async () => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: 'plumas-kmeans'
+      database: process.env.DB_NAME || 'plumas-kmeans',
+    },
+  });
+
+  db.transaction(async (txn) => {
+    try {
+      const osrm = new OSRM(process.env.OSRM_FILE || './data/california-latest.osrm');
+      const treatments: Treatment[] = await txn.table('treatments');
+      const results: TreatedCluster[] = [];
+
+      const clusters: Cluster[] = await txn
+        .table('clusters')
+        .select('*')
+        // tslint:disable-next-line: space-before-function-paren
+        .whereNotExists(function () {
+          this.select('*').from('treatedclusters').whereRaw(`clusters.id = cluster_no`);
+        })
+        .orderByRaw('RANDOM()')
+        .limit(1);
+      const clusterId = clusters[0].id;
+      const pixelsInCluster: Pixel[] = await txn.table('pixels').where({ cluster_no: clusterId });
+      await Promise.all(
+        treatments.map(async (treatment: Treatment) => {
+          const outputs = await processCluster(
+            pixelsInCluster,
+            treatment.id,
+            treatment.name,
+            osrm,
+            txn
+          )
+            .then((res) => {
+              console.log(`pushing results of ${clusterId}, ${treatment.name}`);
+              results.push(res);
+            })
+            .catch((err) => {
+              console.log(`cannot push results of ${clusterId}, ${treatment.name}: ${err.message}`);
+            });
+        })
+      );
+      console.log('inserting into db...');
+      await txn.table('treatedclusters').insert(results);
+      txn.commit();
+    } catch (err) {
+      console.log('------------\n');
+      console.log(err.message);
+      console.log('/n');
+      txn.rollback();
+    } finally {
+      console.log('destroying pg...');
+      txn.destroy(); // TODO: needed?
+      db.destroy();
+      const t1 = performance.now();
+      console.log('Running took ' + (t1 - t0) + ' milliseconds.');
     }
   });
-  try {
-    const osrm = new OSRM('./data/california-latest.osrm');
-    const treatments: Treatment[] = await db.table('treatments');
-    const results: TreatedCluster[] = [];
-
-    const clusters: Cluster[] = await db
-      .table('clusters')
-      .select('*')
-      // tslint:disable-next-line: space-before-function-paren
-      .whereNotExists(function() {
-        this.select('*')
-          .from('treatedclusters')
-          .whereRaw(`clusters.id = cluster_no`);
-      })
-      .orderByRaw('RANDOM()')
-      .limit(1);
-    const clusterId = clusters[0].id;
-    const pixelsInCluster: Pixel[] = await db.table('pixels').where({ cluster_no: clusterId });
-    await Promise.all(
-      treatments.map(async (treatment: Treatment) => {
-        const outputs = await processCluster(
-          pixelsInCluster,
-          treatment.id,
-          treatment.name,
-          osrm,
-          db
-        )
-          .then(res => {
-            console.log(`pushing results of ${clusterId}, ${treatment.name}`);
-            results.push(res);
-          })
-          .catch(err => {
-            console.log(`cannot push results of ${clusterId}, ${treatment.name}: ${err.message}`);
-          });
-      })
-    );
-    console.log('inserting into db...');
-    await db('treatedclusters').insert(results);
-  } catch (err) {
-    console.log('------------\n');
-    console.log(err.message);
-    console.log('/n');
-  } finally {
-    console.log('destroying pg...');
-    db.destroy();
-    const t1 = performance.now();
-    console.log('Running took ' + (t1 - t0) + ' milliseconds.');
-  }
 };
 
 processAllTreatments();
