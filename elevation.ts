12417f927ef4ea90d5caf03ec4878ebc068cb3de
Scott Kirkland <srkirkland@gmail.com>
2021-06-01 13:20:45 -0700

    new way of processing elevation

--- elevation.ts
+++ elevation.ts
@@ -1,16 +1,32 @@
-import { TileSet } from 'srtm-elevation';
-
-// TODO: preload dataset w/ california hgt data
-const tileset = new TileSet(process.env.HGT_FILES || './data/');
+import { SyncTileSet } from 'srtm-elevation';
 
 export const getElevation = (lat: number, lng: number): Promise<number> => {
+  const latFloor = Math.floor(lat);
+  const lngFloor = Math.floor(lng);
   return new Promise((resolve, reject) => {
-    tileset.getElevation([lat, lng], (err: any, elevation: number) => {
-      if (err) {
-        reject('getElevation failed: ' + err.message);
-      } else {
+    const tileset = new SyncTileSet(
+      process.env.HGT_FILES || './data/',
+      [latFloor, lngFloor],
+      [latFloor + 1, lngFloor + 1],
+      (err: any) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+          return;
+        }
+
+        // All tiles are loaded (or downloaded, if they were not already on disk)
+        // and queries can be made synchronous.
+
+        const elevation = tileset.getElevation([lat, lng]);
+        console.log(elevation);
+
         resolve(elevation);
+      },
+      {
+        username: process.env.HGT_USER || 'srkirkland',
+        password: process.env.HGT_PASS,
       }
-    });
+    );
   });
 };
