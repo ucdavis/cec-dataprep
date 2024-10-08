682088a7c509ce2466144716308040c246ecf09a
aunshx <aunsh.sb@gmail.com>
2024-10-07 20:18:11 -0700

    user_details table

--- sql/db_tables.sql
+++ sql/db_tables.sql
@@ -149,3 +149,19 @@ create table url (
     transport_inputs varchar(10000) not null,
     short_url varchar(100) not null
 );
+
+CREATE TABLE user_details (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(1000) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    organization VARCHAR(1000) NOT NULL,
+    org_type VARCHAR(100) NOT NULL,
+    org_website VARCHAR(10000),
+    job_title VARCHAR(255) NOT NULL,
+    linkedin VARCHAR(1000),
+    expertise VARCHAR(100),
+    about_me TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_user_details_email ON user_details(email);
