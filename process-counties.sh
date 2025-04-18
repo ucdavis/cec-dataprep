a8eababe871a5978edb70416ad3ba03944a08ca4
aunshx <aunsh.sb@gmail.com>
2025-04-17 21:35:45 -0700

    Changes

--- process-counties.sh
+++ process-counties.sh
@@ -11,10 +11,6 @@
 #SBATCH --account=adamgrp
 #SBATCH --partition=high
 
-module load node-js/20.15.1-scagana
-module load npm/10.9.0-4tdev2b
-module load conda
-module load python/3.13.0-xf4zxxe
 
 export OSRM_FILE="/scratch/cecdss/california-latest.osrm"
 export INPUT_FOLDER="./data/unprocessed_counties"
@@ -23,15 +19,15 @@ export PROCESSED_FOLDER="./data/processed_files"
 YEAR=${1:-2030}
 
 declare -a COUNTIES=(
-  "alameda" "alpine" "amador" "butte" "calaveras" "colusa" "contra_costa"
-  "del_norte" "el_dorado" "fresno" "glenn" "humboldt" "imperial" "inyo"
-  "kern" "kings" "lake" "lassen" "los_angeles" "madera" "marin" "mariposa"
-  "mendocino" "merced" "modoc" "mono" "monterey" "napa" "nevada" "orange"
-  "placer" "plumas" "riverside" "sacramento" "san_benito" "san_bernardino"
-  "san_diego" "san_francisco" "san_joaquin" "san_luis_obispo" "san_mateo"
-  "santa_barbara" "santa_clara" "santa_cruz" "shasta" "sierra" "siskiyou"
-  "solano" "sonoma" "stanislaus" "sutter" "tehama" "trinity" "tulare"
-  "tuolumne" "ventura" "yolo" "yuba" "no_county"
+  "Alameda" "Alpine" "Amador" "Butte" "Calaveras" "Colusa" "Contra_Costa"
+  "Del_Norte" "El_Dorado" "Fresno" "Glenn" "Humboldt" "Imperial" "Inyo"
+  "Kern" "Kings" "Lake" "Lassen" "Los_Angeles" "Madera" "Marin" "Mariposa"
+  "Mendocino" "Merced" "Modoc" "Mono" "Monterey" "Napa" "Nevada" "Orange"
+  "Placer" "Plumas" "Riverside" "Sacramento" "San_Benito" "San_Bernardino"
+  "San_Diego" "San_Francisco" "San_Joaquin" "San_Luis_Obispo" "San_Mateo"
+  "Santa_Barbara" "Santa_Clara" "Santa_Cruz" "Shasta" "Sierra" "Siskiyou"
+  "Solano" "Sonoma" "Stanislaus" "Sutter" "Tehama" "Trinity" "Tulare"
+  "Tuolumne" "Ventura" "Yolo" "Yuba" "No_County"
 )
 
 COUNTY=${COUNTIES[$SLURM_ARRAY_TASK_ID]}
